42887071bb41eeccf9360d108957969c0dae2169
Marifad <fadeevamarija@gmail.com>
2022-07-16 18:57:59 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int ticket = 900;
+        int rubles = 20;
+        int miles = ticket / rubles;
+
+        System.out.println("Начислено " + miles + " мил(ь/и)");
+    }
+}
+
