547c0f0a354b3d7017311d6d3918e8d81da9812e
Marifad <fadeevamarija@gmail.com>
2022-07-18 12:11:42 +0300

    Added method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
-
     public static void main(String[] args) {
-
-        int ticket = 900;
-        int rubles = 20;
-        int miles = ticket / rubles;
-
-        System.out.println("Начислено " + miles + " мил(ь/и)");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
 
